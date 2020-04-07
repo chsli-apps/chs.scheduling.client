01fdb852e4c58ff6775efa9b4e6888cc2924f4b0
Wade Jakahi <wjakahi@gmail.com>
2020-04-07 11:25:48 -0400

    gh-pages fix

---
+++ precache-manifest.1fa55871cf69a20dc29add44e29a0ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fae2bdd1dc479996b08f",
+    "url": "/js/chunk-vendors.b580ebce.js"
+  },
+  {
+    "revision": "c82302acc6ce81af1a18",
+    "url": "/js/app.b504676a.js"
+  },
+  {
+    "revision": "ddf5e472a2d4da66ea9a02466207a1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae2bdd1dc479996b08f",
+    "url": "/css/chunk-vendors.e88bf7ea.css"
+  }
+];

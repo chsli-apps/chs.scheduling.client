8754000f9c9c17110ba5f7067788237899f0fcbb
Wade Jakahi <wjakahi@gmail.com>
2020-04-08 16:24:11 -0400

    added instructions, disabled editing, emails

---
+++ precache-manifest.52d828a28e37796abe9d84332f9f2d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chs.scheduling.client/robots.txt"
+  },
+  {
+    "revision": "2ccb0bc16cd7ac2f382b",
+    "url": "/chs.scheduling.client/js/chunk-vendors.6f777f32.js"
+  },
+  {
+    "revision": "013d6c146705dbc8d188",
+    "url": "/chs.scheduling.client/js/app.e128f866.js"
+  },
+  {
+    "revision": "b67beecbae346a0a430426c2eeb5f6c8",
+    "url": "/chs.scheduling.client/index.html"
+  },
+  {
+    "revision": "2ccb0bc16cd7ac2f382b",
+    "url": "/chs.scheduling.client/css/chunk-vendors.699e91b2.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chs.scheduling.client/precache-manifest.23feb3dfe9f15e96b93b4c34dcfd9ad6.js"
+  "/chs.scheduling.client/precache-manifest.52d828a28e37796abe9d84332f9f2d3c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chs.scheduling"});
